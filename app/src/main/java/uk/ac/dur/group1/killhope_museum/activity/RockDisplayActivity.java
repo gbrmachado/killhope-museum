f73de863a0d9367c52f5273023743bdd2e2b9444
gbrmachado <gbrmachado@gmail.com>
2015-03-15 18:10:20 +0000

    Second Draft: Added background, text formatting

--- app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/RockDisplayActivity.java
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/RockDisplayActivity.java
@@ -1,20 +1,56 @@
 package uk.ac.dur.group1.killhope_museum.activity;
 
+import android.app.ActionBar;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import uk.ac.dur.group1.killhope_museum.R;
 
+
 public class RockDisplayActivity extends ActionBarActivity {
+//simulating the public class:
+
+    String title = "Ankerite";
+    String formula = "CaCO3";
+    String info = "Ankerite is a calcium, iron, magnesium, manganese carbonate mineral of the " +
+            "group of rhombohedral carbonates with formula: Ca(Fe,Mg,Mn)(CO3)2. In composition it is " +
+            "closely related to dolomite, but differs from this in having magnesium replaced by " +
+            "varying amounts of iron(II) and manganese.";
+    String image = "/assets/Rocks/ankerite.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rock_display);
+        this.populate();
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.ab_gradient));
     }
 
+
+    public void populate() {
+        getSupportActionBar().setTitle(title);    //set the title
+
+     //   getSupportActionBar().setIcon(getResources().getDrawable(R.drawable.ic_menu_logo));
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        image.setImageResource(R.drawable.ankerite);
+
+        TextView textFormula = (TextView) findViewById(R.id.chemFormulaInfo);
+        textFormula.setText(formula);
+
+        TextView textTitle = (TextView) findViewById(R.id.textInfo);
+        textTitle.setText(info);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,6 +58,15 @@ public class RockDisplayActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_rock_display, menu);
         return true;
     }
+}
+
+
+//TODO: PUT IMAGES ON ACTION MENU
+//TODO: BACKGROUND COLORS(improve it)
+//TODO: IMAGE'S GALLERY
+//TODO: TEST WITH SOME REAL INFO
+    /*
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -38,3 +83,4 @@ public class RockDisplayActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+*/
