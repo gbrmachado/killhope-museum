5e3efab4778c94d243467cb3c1b3d05fba873399
David-Allison <david.allison2@durham.ac.uk>
2015-03-21 02:29:50 +0000

    Updated Application to use custom class

---
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/KillhopeApplication.java
@@ -0,0 +1,11 @@
+package uk.ac.dur.group1.killhope_museum;
+
+import android.app.Application;
+
+/**
+ * A class designed to hold global variables for the Killhope museum application.
+ */
+public class KillhopeApplication extends Application
+{
+
+}
