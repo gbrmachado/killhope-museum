98d4e8de4f63bc0eedf702c467b31f62f6914177
David-Allison <david.allison2@durham.ac.uk>
2015-04-01 12:46:24 +0100

    Added rock validation to KillhopeApplication

--- app/src/main/java/uk/ac/dur/group1/killhope_museum/KillhopeApplication.java
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/KillhopeApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import java.util.List;
 
+import uk.ac.dur.group1.killhope_museum.activity.RockDisplayActivity;
 import uk.ac.dur.group1.killhope_museum.activity.RockListActivity;
 import uk.ac.dur.group1.killhope_museum.dto.RockDTO;
 import uk.ac.dur.group1.killhope_museum.dto.RockListFacade;
@@ -13,16 +14,65 @@ import uk.ac.dur.group1.killhope_museum.dto.RockListFacade;
  */
 public class KillhopeApplication extends Application
 {
+    private List<RockDTO> rocks = null;
+
     @Override
     public void onCreate()
     {
         super.onCreate();
     }
 
-    public List<RockDTO> getRockList()
+    private void initialise()
     {
-        return RockListFactory.getRockList(this.getResources());
+
+        if(rocks == null)
+            rocks = RockListFactory.getRockList(this.getResources());
     }
 
+
+    public List<RockDTO> getRockList()
+    {
+        initialise();
+
+        return rocks;  //Just hope and pray that the consumer won't mutate them.
+    }
+
+
+
+
+    public boolean isValidRockID(String id)
+    {
+        initialise();
+        if(id == null)
+            return false;
+
+        for(RockDTO rock : rocks)
+        {
+            if(rock == null)
+                continue;
+            String obtainedID = rock.getID().toString();
+            if (id.equals(obtainedID)) //id is guaranteed not to be null.
+                return true;
+        }
+        return false;
+    }
+
+    public RockDTO getRock(String id)
+    {
+        initialise();
+        if(id == null)
+            throw new IllegalArgumentException("id is null");
+
+        for(RockDTO rock : rocks)
+        {
+            if(rock == null)
+                continue;
+            String obtainedID = rock.getID().toString();
+            if (id.equals(obtainedID)) //id is guaranteed not to be null.
+                return rock;
+        }
+
+        throw new IllegalStateException("rock not found");
+    }
 
 }
