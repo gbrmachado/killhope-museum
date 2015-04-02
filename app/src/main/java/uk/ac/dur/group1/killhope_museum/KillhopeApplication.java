2ee3e8a0862859dc1ee8baa3c5802f5b10cf31d8
David-Allison <david.allison2@durham.ac.uk>
2015-04-02 10:43:48 +0100

    Added map loading to application

--- app/src/main/java/uk/ac/dur/group1/killhope_museum/KillhopeApplication.java
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/KillhopeApplication.java
@@ -1,12 +1,15 @@
 package uk.ac.dur.group1.killhope_museum;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import java.util.List;
 
-import uk.ac.dur.group1.killhope_museum.activity.RockListActivity;
+import uk.ac.dur.group1.killhope_museum.dto.MapDTO;
+import uk.ac.dur.group1.killhope_museum.dto.json.JsonMap;
 import uk.ac.dur.group1.killhope_museum.dto.RockDTO;
-import uk.ac.dur.group1.killhope_museum.dto.RockListFacade;
+import uk.ac.dur.group1.killhope_museum.utilities.JSONUtilities;
 
 /**
  * A class designed to hold global variables for the Killhope museum application.
@@ -24,5 +27,27 @@ public class KillhopeApplication extends Application
         return RockListFactory.getRockList(this.getResources());
     }
 
+    public MapDTO getMap()
+    {
+        String json = JSONUtilities.getJsonFromRaw(getResources(), R.raw.maps);
+        JsonMap map =  JsonMap.fromJson(json);
+
+        Bitmap image = imageforMap(map);
+        return new MapDTO(map,image);
+    }
+
+    private Bitmap imageforMap(JsonMap map)
+    {
+        if(map.getImage() == null || map.getImage() == "")
+            return getDefaultMapImage();
+
+        //TODO: Ben
+        throw new UnsupportedOperationException("Not implemented - TODO: Ben");
+    }
+
+    private Bitmap getDefaultMapImage()
+    {
+        return BitmapFactory.decodeResource(getResources(), R.drawable.killhope_map);
+    }
 
 }
