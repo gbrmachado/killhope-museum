a98239708d66810fed47b039b357ae46dfc14bb6
David-Allison <david.allison2@durham.ac.uk>
2015-04-04 15:17:38 +0100

    Fix fullscreenimage

--- app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/FullScreenImageActivity.java
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/FullScreenImageActivity.java
@@ -6,19 +6,23 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.AttributeSet;
 import android.util.Base64;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
@@ -66,114 +70,37 @@ public class FullScreenImageActivity extends Activity {
         context.startActivity(myIntent);
     }
 
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+    private static boolean destroyedViaRotation = false;
 
     /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
+     * The number of milliseconds to wait after user interaction before hiding the system UI.
      */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+    private static final int AUTO_HIDE_DELAY_MILLIS = 2000;
 
     /**
      * The key of the required parcelable for this activity.
      */
     public static final String IMAGE_KEY = "image";
 
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
+    private int myOrientation;
+    private View controlsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FullScreenImageActivity.cachedValue = null;
+        final FullScreenImageActivity self = this;
 
         setContentView(R.layout.activity_full_screen_image);
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);//the view to hide.
-        final View contentView = findViewById(R.id.full_screen_content_view);
+        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+        myOrientation = display.getRotation();
 
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
 
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                //fudge factor as in later versions of android, once the navigation
-                                //is hidden, the height of the control changes.
-                                mControlsHeight = controlsView.getHeight() + 300;
+        controlsView = findViewById(R.id.fullscreen_content_controls);
 
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
+        ImageView close = (ImageView) findViewById(R.id.full_screen_close);
 
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
 
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-
-        ImageView close = (ImageView)findViewById(R.id.full_screen_close);
-        close.setOnTouchListener(mDelayHideTouchListener);
-
-        final FullScreenImageActivity self = this;
         close.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -182,8 +109,7 @@ public class FullScreenImageActivity extends Activity {
         });
 
 
-        if(cachedValue == null)
-        {
+        if (cachedValue == null) {
             int resourceID = this.getIntent().getIntExtra(IMAGE_KEY, -1);
             if (resourceID == -1)
                 throw new IllegalArgumentException("Invalid/no key set: " + IMAGE_KEY);
@@ -191,16 +117,17 @@ public class FullScreenImageActivity extends Activity {
             cachedValue = BitmapFactory.decodeResource(this.getResources(), resourceID);
         }
 
-        this.loadImage(cachedValue);
+        FrameLayout fscw = (FrameLayout) findViewById(R.id.full_screen_content_view);
+        TouchWebView a = new TouchWebView(this);
+        fscw.addView(a);
+        this.loadImage(a, cachedValue);
     }
-
-    private void loadImage(Bitmap image)
+    private void loadImage(WebView v, Bitmap image)
     {
         //convert the image to base64 so we don't need to deal with more problems with relative URLs
         //if we convert this to a server-based interface.
         String encodedImage = BitmapUtilities.convertToBase64(image);
 
-        WebView v = (WebView)this.findViewById(R.id.full_screen_image_web_view);
         String data = getHTML(encodedImage);
         v.loadData(data, "text/html", "UTF-8");
 
@@ -223,8 +150,9 @@ public class FullScreenImageActivity extends Activity {
             width = display.getWidth();
             height = display.getHeight();
         }
+
+        //settings.setSupportZoom(true);
         settings.setBuiltInZoomControls(true);
-        settings.setSupportZoom(true);
 
         //scroll to the middle.
         //TODO: Check with someone that this is the correct way to do so.
@@ -261,30 +189,14 @@ public class FullScreenImageActivity extends Activity {
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
         // are available.
-        delayedHide(100);
+        delayedHide(1000);
     }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     Handler mHideHandler = new Handler();
     Runnable mHideRunnable = new Runnable() {
         @Override
         public void run() {
-            mSystemUiHider.hide();
+            controlsView.setVisibility(View.GONE);
         }
     };
 
@@ -299,7 +211,72 @@ public class FullScreenImageActivity extends Activity {
 
     @Override
     protected void onDestroy() {
+
+        if(!FullScreenImageActivity.destroyedViaRotation)
+            FullScreenImageActivity.cachedValue = null;
+        FullScreenImageActivity.destroyedViaRotation = false;
+
         super.onDestroy();
-        FullScreenImageActivity.cachedValue = null;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if(newConfig.orientation != myOrientation)
+            FullScreenImageActivity.destroyedViaRotation = true;
+
+        super.onConfigurationChanged(newConfig);
+    }
+
+
+    //http://stackoverflow.com/a/21173178/1112560
+    public class TouchWebView extends WebView {
+
+        WebTouchListener wtl;
+
+        public TouchWebView(Context context) {
+            super(context);
+        }
+
+        public TouchWebView(Context context, AttributeSet attrs, int defStyle) {
+            super(context, attrs, defStyle);
+        }
+
+        public TouchWebView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public void resetTouchListener() {
+
+            if(this.wtl == null) {
+                this.wtl = new WebTouchListener();
+            }
+            this.setOnTouchListener(wtl);
+        }
+
+        @Override
+        public boolean dispatchTouchEvent(MotionEvent event) {
+
+            this.resetTouchListener();
+            return super.dispatchTouchEvent(event);
+        }
+
+        public class WebTouchListener implements OnTouchListener {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                WebView.HitTestResult hr = ((TouchWebView)v).getHitTestResult();
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    controlsView.setVisibility(View.VISIBLE);
+                    delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                }
+
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+
+        }
     }
 }
