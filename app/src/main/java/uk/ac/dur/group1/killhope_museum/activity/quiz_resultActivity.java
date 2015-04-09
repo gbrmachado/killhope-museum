ddb691495de4348c0f8d80dce236ad99be65017f
David-Allison <david.allison2@durham.ac.uk>
2015-04-09 15:55:29 +0100

    (minor) Extracted functions, extracted constants

--- app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/quiz_resultActivity.java
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/quiz_resultActivity.java
@@ -14,38 +14,54 @@ import uk.ac.dur.group1.killhope_museum.R;
 /**
  * Created by haijiewu on 28/03/15.
  */
-public class quiz_resultActivity extends Activity{
+public class quiz_resultActivity extends Activity
+{
+    public static final String INTENT_PARAM_SCORE = "Score";
+    public static final String INTENT_PARAM_CORRECT_ANSWERS = "selectedWrong";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_result);
-        int num = getIntent().getExtras().getInt("Score");
-        // first text
-        TextView textView_one = (TextView) findViewById(R.id.text_one);
-        String text = String.format(getString(R.string.quiz_score_explanation), num);
-        textView_one.setText(text);
-        // second text
-        TextView textView_two = (TextView) findViewById(R.id.text_two);
-        if(num >= 2){
-            textView_two.setText(getString(R.string.quiz_try_harder_level));
-        }else{
-            textView_two.setText(getString(R.string.quiz_try_again));
-        }
-        // third text, display correct answers
-        ArrayList<String> answer = getIntent().getExtras().getStringArrayList("selectedWrong");
+
+        int score = getIntent().getExtras().getInt(INTENT_PARAM_SCORE);
+
+        displayScoreAmount(score);
+        displayScoreComment(score);
+        displayCorrectAnswers();
+    }
+
+    private void displayCorrectAnswers() {
+        ArrayList<String> answer = getIntent().getExtras().getStringArrayList(INTENT_PARAM_CORRECT_ANSWERS);
         String display = getString(R.string.quiz_incorrect_answer_display) +"\n\n";
         int count = 0;
         for(String item:answer){
-            display = display + item + "\n";
-            // make space between each questions
+            display += item + "\n";
+            // Display a blank line between every answer
+            // Answer contains 2 items per question, the question and the correct item.
             count++;
             if(count == 2){
-                display = display + "\n";
+                display += "\n";
                 count = 0;
             }
         }
         TextView textView_three = (TextView) findViewById(R.id.text_three);
         textView_three.setText(display);
+    }
+
+    private void displayScoreAmount(int score) {
+        TextView textView_one = (TextView) findViewById(R.id.text_one);
+        String text = String.format(getString(R.string.quiz_score_explanation), score);
+        textView_one.setText(text);
+    }
+
+    private void displayScoreComment(int score) {
+        TextView textView_two = (TextView) findViewById(R.id.text_two);
+        if(score >= 2){
+            textView_two.setText(getString(R.string.quiz_try_harder_level));
+        }else{
+            textView_two.setText(getString(R.string.quiz_try_again));
+        }
     }
 
     @Override
