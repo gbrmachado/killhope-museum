9ac6bd66b2860b97b76252a85dd61a2981729e71
David-Allison <david.allison2@durham.ac.uk>
2015-04-09 21:49:40 +0100

    Removed Action bar

--- app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/SplashScreenActivity.java
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/SplashScreenActivity.java
@@ -4,7 +4,6 @@ import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -52,24 +51,7 @@ public class SplashScreenActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
