4a121173c75a53b6567cfb9bfc8170b2ce2e0def
David-Allison <david.allison2@durham.ac.uk>
2015-04-11 19:57:03 +0100

    Added link handling

--- app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/QRCodeActivity.java
+++ app/src/main/java/uk/ac/dur/group1/killhope_museum/activity/QRCodeActivity.java
@@ -2,25 +2,29 @@ package uk.ac.dur.group1.killhope_museum.activity;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import java.util.regex.Pattern;
 
 import uk.ac.dur.group1.killhope_museum.KillhopeApplication;
 import uk.ac.dur.group1.killhope_museum.R;
 
 
 public class QRCodeActivity extends ActionBarActivity {
+
+    private static final Pattern withHTTPS = Pattern.compile("(https?://)?(www\\.)?killhope.org.uk.*");
+    private static final Pattern withoutHTTPS = Pattern.compile("(http://)?(www\\.)?killhope.org.uk.*");
+    private static final boolean useHTTPS = false;
 
     private ImageButton mButton;
 
@@ -71,17 +75,46 @@ public class QRCodeActivity extends ActionBarActivity {
 
         if(!app.isValidRockID(potentialRockID))
         {
-            new AlertDialog.Builder(this)
-                    .setTitle("Error")
-                    .setMessage(String.format("The supplied asset ID: %s was not found in the database.", potentialRockID))
-                    .setCancelable(false).setPositiveButton("ok", null)
-                    .create().show();
-            return; //let the user try again, or back out.
+            if(isValidURL(potentialRockID))
+            {
+                handleURL(potentialRockID);
+                finish();
+                return;
+            }
+            else {
+                new AlertDialog.Builder(this)
+                        .setTitle("Error")
+                        .setMessage(String.format("The supplied asset ID: %s was not found in the database.", potentialRockID))
+                        .setCancelable(false).setPositiveButton("ok", null)
+                        .create().show();
+                return; //let the user try again, or back out.
+            }
         }
 
         finish();
         RockDisplayActivity.launchActivity(this, app.getRock(potentialRockID));
 
+    }
+
+    private void handleURL(String potentialRockID) {
+
+        if(!potentialRockID.startsWith("http://") && !(useHTTPS && potentialRockID.startsWith("https://")))
+        {
+            String prefix = useHTTPS ? "https://" : "http://";
+            potentialRockID = prefix + potentialRockID;
+        }
+
+        Uri uri = Uri.parse(potentialRockID);
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(browserIntent);
+    }
+
+
+
+    private boolean isValidURL(String potentialRockID)
+    {
+        Pattern p = useHTTPS ? withHTTPS : withoutHTTPS;
+        return p.matcher(potentialRockID).find();
     }
 
     @Override
